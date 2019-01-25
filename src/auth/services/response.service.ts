0803ba6476db61ef593d003ec83fdffc26b9b69f
Hamza El Meknassi <elmeknassi.h@gmail.com>
2019-01-25 11:08:02 +0100

    Added doc for ResponseService

--- src/auth/services/response.service.ts
+++ src/auth/services/response.service.ts
@@ -2,6 +2,10 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class ResponseService {
+  /**@function badLogin
+   * @description Throws the exception of a login failure
+   * @returns {HttpException}
+   */
   badLogin(): HttpException {
     throw new HttpException({
       status: HttpStatus.UNAUTHORIZED,
